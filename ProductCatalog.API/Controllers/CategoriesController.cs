f8968b061bf6aa6cc36e4e8c8db33df56e6e4f7e
PreeyaPatil <patilpreeya1008@gmail.com>
2026-04-13 17:42:21 -0500

    Add CategoriesController GET endpoint

---
+++ ProductCatalog.API/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductCatalog.API.Models;
+
+namespace ProductCatalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(new[]
+            {
+                new Category { Id = 1, Title = "Electronics" },
+                new Category { Id = 2, Title = "Accessories" }
+            });
+
+        }
+    }
+}
