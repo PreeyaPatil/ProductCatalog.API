3a471549e6fb601fbf9b2ce066ab3fd761fcd69f
PreeyaPatil <patilpreeya1008@gmail.com>
2026-04-14 08:40:14 -0500

    Configure JSON serialization to keep C# property names

--- ProductCatalog.API/Program.cs
+++ ProductCatalog.API/Program.cs
@@ -11,7 +11,10 @@ namespace ProductCatalog.API
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
